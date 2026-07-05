88fc3b6d85064a62de8b03d2c3a056161db9d45f
Manuel Guarniz <cruzemg95@gmail.com>
2026-07-05 15:48:58 -0500

    update css

--- src/main/java/pe/edu/utp/appcasaforno/App.java
+++ src/main/java/pe/edu/utp/appcasaforno/App.java
@@ -2,7 +2,7 @@ package pe.edu.utp.appcasaforno;
 
 import pe.edu.utp.appcasaforno.presentation.ServerController;
 
-public class HelloWorld {
+public class App {
 
     public static void main(String[] args) throws Exception {
         var server = new ServerController();
